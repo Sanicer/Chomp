c365f526ba5cc172f90ed063dc9e1be3f0720540
SanicLaptop <SanicLaptop@DESKTOP-849SAJE>
2017-03-21 15:05:14 +0800

    Add project files.

---
+++ Chomp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Chomp.Startup))]
+namespace Chomp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
